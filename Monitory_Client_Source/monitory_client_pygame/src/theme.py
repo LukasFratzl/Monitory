375f30cbf1124557a38c1f843a148d94089ca02e
flow____ <socials.finished253@passmail.com>
2026-05-30 17:41:21 +0200

    Light Theme done

--- Monitory_Client_Source/monitory_client_pygame/src/theme.py
+++ Monitory_Client_Source/monitory_client_pygame/src/theme.py
@@ -197,75 +197,135 @@ class LightTheme(DarkTheme):
         self.time_slice.font_bg_color = self.none
         # Thats the line between tima and date
         self.time_slice.graph_bottom_line_color = self.color_dark_gray
+        self.time_slice.label_font = pygame.font.Font('assets/ttf/FiraCode-Bold.ttf', int(self.font_size * 1.25))
         
         self.date_slice = AppThemeSlice()
         self.date_slice.font_color = self.color_dark_gray
         self.date_slice.font_bg_color = self.none
+        self.date_slice.label_font = self.default_font
         
         self.cpu_slice = AppThemeSlice()
         self.cpu_slice.font_color = self.color_dark_gray
         self.cpu_slice.font_bg_color = self.none
+        self.cpu_slice.perc_font_color = self.color_dark_gray
+        self.cpu_slice.perc_font_bg_color = self.none
+        self.cpu_slice.stat_font_color = self.color_dark_gray
+        self.cpu_slice.stat_font_bg_color = self.none
         self.cpu_slice.graph_average_line_color = self.color_pink
         self.cpu_slice.graph_bottom_line_color = self.color_transparent
-        self.cpu_slice.graph_bg_color = self.color_purple_half
+        self.cpu_slice.graph_bg_color = self.color_blue_gray_half
         self.cpu_slice.graph_line_color = self.color_transparent
+        self.cpu_slice.label_font = self.default_font
+        self.cpu_slice.stats_0_font = self.default_font_stat_0
+        self.cpu_slice.stats_1_font = self.default_font_stat_1
     
         self.dram_slice = AppThemeSlice()
         self.dram_slice.font_color = self.color_dark_gray
         self.dram_slice.font_bg_color = self.none
+        self.dram_slice.perc_font_color = self.color_dark_gray
+        self.dram_slice.perc_font_bg_color = self.none
+        self.dram_slice.stat_font_color = self.color_dark_gray
+        self.dram_slice.stat_font_bg_color = self.none
         self.dram_slice.graph_average_line_color = self.color_transparent
         self.dram_slice.graph_bottom_line_color = self.color_transparent
-        self.dram_slice.graph_bg_color = self.color_purple_half
+        self.dram_slice.graph_bg_color = self.color_blue_gray_half
         self.dram_slice.graph_line_color = self.color_green
+        self.dram_slice.label_font = self.default_font
+        self.dram_slice.stats_0_font = self.default_font_stat_0
+        self.dram_slice.stats_1_font = self.default_font_stat_1
         
         self.disk_slice = AppThemeSlice()
         self.disk_slice.font_color = self.color_dark_gray
         self.disk_slice.font_bg_color = self.none
+        self.disk_slice.perc_font_color = self.color_dark_gray
+        self.disk_slice.perc_font_bg_color = self.none
+        self.disk_slice.stat_font_color = self.color_dark_gray
+        self.disk_slice.stat_font_bg_color = self.none
         self.disk_slice.graph_average_line_color = self.color_transparent
         self.disk_slice.graph_bottom_line_color = self.color_transparent 
-        self.disk_slice.graph_bg_color = self.color_purple_half
-        self.disk_slice.graph_line_color = self.color_green
+        self.disk_slice.graph_bg_color = self.color_blue_gray_half
+        self.disk_slice.graph_line_color = self.color_transparent
+        self.disk_slice.label_font = self.default_font
+        self.disk_slice.stats_0_font = self.default_font_stat_0
+        self.disk_slice.stats_1_font = self.default_font_stat_1
         
         self.gpu_slice = AppThemeSlice()
         self.gpu_slice.font_color = self.color_dark_gray
         self.gpu_slice.font_bg_color = self.none
+        self.gpu_slice.perc_font_color = self.color_dark_gray
+        self.gpu_slice.perc_font_bg_color = self.none
+        self.gpu_slice.stat_font_color = self.color_dark_gray
+        self.gpu_slice.stat_font_bg_color = self.none
         self.gpu_slice.graph_average_line_color = self.color_transparent
         self.gpu_slice.graph_bottom_line_color = self.color_transparent 
-        self.gpu_slice.graph_bg_color = self.color_purple_half
+        self.gpu_slice.graph_bg_color = self.color_blue_gray_half
         self.gpu_slice.graph_line_color = self.color_green
+        self.gpu_slice.label_font = self.default_font
+        self.gpu_slice.stats_0_font = self.default_font_stat_0
+        self.gpu_slice.stats_1_font = self.default_font_stat_1
         
         self.vram_slice = AppThemeSlice()
         self.vram_slice.font_color = self.color_dark_gray
         self.vram_slice.font_bg_color = self.none
+        self.vram_slice.perc_font_color = self.color_dark_gray
+        self.vram_slice.perc_font_bg_color = self.none
+        self.vram_slice.stat_font_color = self.color_dark_gray
+        self.vram_slice.stat_font_bg_color = self.none
         self.vram_slice.graph_average_line_color = self.color_transparent
         self.vram_slice.graph_bottom_line_color = self.color_transparent 
-        self.vram_slice.graph_bg_color = self.color_purple_half
+        self.vram_slice.graph_bg_color = self.color_blue_gray_half
         self.vram_slice.graph_line_color = self.color_green
+        self.vram_slice.label_font = self.default_font
+        self.vram_slice.stats_0_font = self.default_font_stat_0
+        self.vram_slice.stats_1_font = self.default_font_stat_1
         
         self.net_slice = AppThemeSlice()
         self.net_slice.font_color = self.color_dark_gray
         self.net_slice.font_bg_color = self.none
+        self.net_slice.perc_font_color = self.color_net_down
+        self.net_slice.perc_font_bg_color = self.none
+        self.net_slice.stat_font_color = self.color_net_up
+        self.net_slice.stat_font_bg_color = self.none
         self.net_slice.graph_average_line_color = self.color_transparent
         self.net_slice.graph_bottom_line_color = self.color_transparent 
         self.net_slice.graph_bg_color = self.color_transparent
         self.net_slice.graph_line_color = self.color_transparent
+        self.net_slice.label_font = self.default_font
+        self.net_slice.stats_0_font = self.default_font_stat_0
+        self.net_slice.stats_1_font = self.default_font_stat_1
         self.net_slice.line_color_offset = 27
         
         self.watt_slice = AppThemeSlice()
         self.watt_slice.font_color = self.color_dark_gray
         self.watt_slice.font_bg_color = self.none
+        self.watt_slice.perc_font_color = self.color_dark_gray
+        self.watt_slice.perc_font_bg_color = self.none
+        self.watt_slice.stat_font_color = self.color_dark_gray
+        self.watt_slice.stat_font_bg_color = self.none
         self.watt_slice.graph_average_line_color = self.color_transparent
         self.watt_slice.graph_bottom_line_color = self.color_transparent 
-        self.watt_slice.graph_bg_color = self.color_purple_half
-        self.watt_slice.graph_line_color = self.color_green
+        self.watt_slice.graph_bg_color = self.color_transparent
+        self.watt_slice.graph_line_color = self.color_transparent
+        self.watt_slice.label_font = self.default_font
+        self.watt_slice.stats_0_font = self.default_font_stat_0
+        self.watt_slice.stats_1_font = self.default_font_stat_1
+        self.watt_slice.label_legent_items_font = self.default_label_legent_items_font
         
         self.temp_slice = AppThemeSlice()
         self.temp_slice.font_color = self.color_dark_gray
         self.temp_slice.font_bg_color = self.none
+        self.temp_slice.perc_font_color = self.color_dark_gray
+        self.temp_slice.perc_font_bg_color = self.none
+        self.temp_slice.stat_font_color = self.color_dark_gray
+        self.temp_slice.stat_font_bg_color = self.none
         self.temp_slice.graph_average_line_color = self.color_transparent
         self.temp_slice.graph_bottom_line_color = self.color_transparent 
-        self.temp_slice.graph_bg_color = self.color_purple_half
-        self.temp_slice.graph_line_color = self.color_green
+        self.temp_slice.graph_bg_color = self.color_transparent
+        self.temp_slice.graph_line_color = self.color_transparent
+        self.temp_slice.label_font = self.default_font
+        self.temp_slice.stats_0_font = self.default_font_stat_0
+        self.temp_slice.stats_1_font = self.default_font_stat_1
+        self.temp_slice.label_legent_items_font = self.default_label_legent_items_font
         
 class Theme:
     def __init__(self):
